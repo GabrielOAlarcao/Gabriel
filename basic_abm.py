7254e518378d07a56837931166927060f05a6908
GabrielOAlarcao <gabriel.alarcao@gmail.com>
2024-01-19 21:39:59 -0300

    Adding Monte Carlo Simulation to the basic model

--- basic_abm.py
+++ basic_abm.py
@@ -4,78 +4,149 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class FirmSimulation:
-    def __init__(self, Ni, Time, gamma, phi, Pbar, delta, rbar, MC):
+    def __init__(self, Ni, Time, gamma, phi, Pbar, r):
         self.Ni = Ni
         self.Time = Time
         self.gamma = gamma
         self.phi = phi
         self.Pbar = Pbar
-        self.delta = delta
-        self.rbar = rbar
-        self.MC = MC
-        self.r = np.ones((Ni, Time, MC))
-        self.A = np.ones((Ni, Time, MC))
-        self.K = np.ones((Ni, Time, MC))
-        self.B = np.zeros((Ni, Time, MC))
-        self.I = np.zeros((Ni, Time, MC))
-        self.P = np.zeros((Ni, Time, MC))
-        self.Y = np.zeros((Ni, Time, MC))
-        self.Z = 2 * np.random.rand(Ni, 1, MC) + Pbar
-        self.YY = np.zeros((Time, MC))
-        self.AA = np.zeros((Time, MC))
-        self.BB = np.zeros((Time, MC))
-
+        self.r = r
+        self.A = np.ones((Ni, 1))
+        self.K = np.ones((Ni, 1))
+        self.B = np.zeros((Ni, 1))
+        self.I = np.zeros((Ni, 1))
+        self.P = np.zeros((Ni, 1))
+        self.Y = np.zeros((Ni, 1))
+        self.Z = 2 * np.random.rand(Ni, 1) + Pbar
+        self.YY = np.zeros((Time, 1))
+        self.AA = np.zeros((Time, 1))
+        self.BB = np.zeros((Time, 1))
+        
     def run_simulation(self):
-        for mc in range(self.MC):
-            for t in range(1, self.Time):
-                self.I[:, t, mc] = self.gamma * self.Z[:, t-1, mc]
-                self.K[:, t, mc] = (1 - self.delta) * self.K[:, t-1, mc] + self.I[:, t, mc]
-                self.Y[:, t, mc] = self.phi * self.K[:, t, mc]
-                self.B[:, t, mc] = self.K[:, t, mc] - self.A[:, t-1, mc]
-                self.B[self.B[:, t, mc] < 0, t, mc] = 0
-                self.P[:, t, mc] = 2 * np.random.rand(self.Ni) + self.Pbar
-                self.Z[:, t, mc] = self.P[:, t, mc] * self.Y[:, t, mc] - self.r[:, t, mc] * self.K[:, t, mc]
-                self.A[:, t, mc] = self.A[:, t-1, mc] + self.Z[:, t, mc]
-                self.r[:, t, mc] = self.rbar + self.rbar * (self.B[:, t, mc] / self.A[:, t, mc]) ** self.rbar
-                self.Z[self.A[:, t, mc] < 0, t, mc] = 0
-                self.I[self.I[:, t, mc] < 0, t, mc] = 0
-                self.K[self.A[:, t, mc] < 0, t, mc] = 1
-                self.A[self.A[:, t, mc] < 0, t, mc] = 1
-                self.YY[t, mc] = np.sum(self.Y[:, t, mc])
-                self.AA[t, mc] = np.sum(self.A[:, t, mc])
-                self.BB[t, mc] = np.sum(self.B[:, t, mc])
-
-    def plot_results(self, variable_name, simulation_index=0):
+        for t in range(1, self.Time):
+            self.I = self.gamma * self.Z
+            self.K = self.K + self.I
+            self.Y = self.phi * self.K
+            self.B = self.K - self.A
+            self.B[self.B < 0] = 0
+            self.P = 2 * np.random.rand(self.Ni, 1) + self.Pbar
+            self.Z = self.P * self.Y - self.r * self.K
+            self.A = self.A + self.Z
+            self.Z[self.A < 0] = 0
+            self.K[self.A < 0] = 1
+            self.A[self.A < 0] = 1
+            self.YY[t] = np.sum(self.Y)
+            self.AA[t] = np.sum(self.A)
+            self.BB[t] = np.sum(self.B)
+            
+    def plot_results(self, variable_name):
         time_steps = np.arange(1, self.Time)
-        variable_values = getattr(self, variable_name)[1:, simulation_index]
-
+        variable_values = getattr(self, variable_name)[1:]
+        
         plt.figure(figsize=(10, 4))
-
-        for mc in range(self.MC):
-          plt.plot(time_steps, variable_values[:, mc], label=f'{variable_name} (Simulation {mc + 1})')
-          
-        plt.title(f'{variable_name} Over Time (All Simulations)')
+        
+        plt.plot(time_steps, variable_values, label=variable_name)
+        plt.title(f'{variable_name} Over Time')
         plt.xlabel('Time')
         plt.ylabel(variable_name)
         plt.legend()
-
+        
         plt.show()
-
 
 # Example usage:
 Ni = 100  # Number of firms
 Time = 1000  # Number of simulations
 gamma = 1.1  # Investment accelerator
-phi = 0.1  # Capital produtivity
+phi = 0.1  # Capital productivity
 Pbar = 0.01  # Price constant
-rbar = 0.075  # Interest rate
-delta = 0.05 # Depreciation rate
+r = 0.1  # Interest rate
 
-simulation = FirmSimulation(Ni, Time, gamma, phi, Pbar, rbar, delta)
+simulation = FirmSimulation(Ni, Time, gamma, phi, Pbar, r)
 simulation.run_simulation()
-
-# Plotting different variables
 simulation.plot_results('YY')  # Aggregate Production
 simulation.plot_results('AA')   # Net Worth
 simulation.plot_results('BB')   # Capital
-# ... Add more as needed
+
+
+## -------------------- Monte Carlo Simulated Model --------------------------- ##
+class MonteCarloFirmSimulation:
+    def __init__(self, Time, Ni, MC, gamma, phi, Pbar, delta, rbar, seed=None):
+        self.Time = Time
+        self.Ni = Ni
+        self.MC = MC
+        self.gamma = gamma
+        self.phi = phi
+        self.Pbar = Pbar
+        self.delta = delta
+        self.rbar = rbar
+
+        # Set random seed for reproducibility
+        np.random.seed(seed)
+
+        # Allocate aggregate variables / initial conditions
+        self.YY = np.zeros((Time, MC))  # aggregate production
+        self.AA = np.zeros((Time, MC))  # aggregate net worth
+        self.BB = np.zeros((Time, MC))  # aggregate debt
+        self.LEV = np.zeros((Time, MC))  # leverage
+
+    def run_simulation(self):
+        for mc in range(self.MC):
+            # Allocate individual variables / initial conditions
+            A = np.ones((self.Ni, 1))  # net worth
+            K = np.ones((self.Ni, 1))  # capital
+            B = np.zeros((self.Ni, 1))  # debt
+            I = np.zeros((self.Ni, 1))  # investment
+            P = np.zeros((self.Ni, 1))  # price
+            Y = np.zeros((self.Ni, 1))  # production
+            Z = 2 * np.random.rand(self.Ni, 1) + self.Pbar  # profit
+
+            # Main program
+            for t in range(1, self.Time):
+                I = self.gamma * Z  # investment choice
+                I[I < 0] = 0
+                K = (1 - self.delta) * K + I  # capital accumulation
+                Y = self.phi * K  # production
+                B = K - A  # debt
+                B[B < 0] = 0  # self-financed firms
+                P = 2 * np.random.rand(self.Ni, 1) + self.Pbar  # stochastic price
+                r = self.rbar + self.rbar * (B / np.maximum(A, 1e-6)) ** self.rbar
+                Z = P * Y - r * K  # profit
+                A = A + Z  # net worth
+                Z[A < 0] = 0  # entry condition
+                K[A < 0] = 1  # entry condition
+                A[A < 0] = 1  # entry-exit process
+                self.YY[t, mc] = np.sum(Y)  # aggregate production
+                self.AA[t, mc] = np.sum(A)  # aggregate net worth
+                self.BB[t, mc] = np.sum(B)  # aggregate debt
+                self.LEV[t, mc] = np.sum(self.BB[t, mc]) / np.sum(self.AA[t, mc])  # average leverage
+
+    def plot_results(self, variable_name):
+        time_steps = np.arange(1, self.Time)
+        variable_values = getattr(self, variable_name)[1:, :]
+        plt.figure(figsize=(10, 6))
+        for j in range(self.MC):
+            plt.plot(time_steps, variable_values[:, j], label=f'Simulation {j + 1}')
+        plt.xlabel('Time')
+        plt.ylabel(f'{variable_name}')
+        plt.title(f'{variable_name} Over Time (Multiple Simulations)')
+        plt.legend()
+        plt.show()
+
+# Set parameters
+Time = 1000  # number of simulation periods
+Ni = 100  # number of firms
+MC = 3  # number of multiple simulations
+gamma = 2  # fraction of reinvested profit
+phi = 0.1  # capital productivity
+Pbar = 0.01  # random price drift
+delta = 0.05  # depreciation rate
+rbar = 0.075  # interest rate
+
+# Create an instance of the ToyModelMC class
+toy_model = MonteCarloFirmSimulation(Time, Ni, MC, gamma, phi, Pbar, delta, rbar, seed=None)
+
+# Run the simulation
+toy_model.run_simulation()
+
+# Plot the results
+toy_model.plot_results('BB')
